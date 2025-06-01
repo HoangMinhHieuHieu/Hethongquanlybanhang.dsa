a9218f7e21f8c7d86774d88db0ee285aac292a1a
Hoang Minh Hieu <minhhieu123tdvp@gmail.com>
2025-06-01 17:16:17 +0700

    Update baocaothongke.h

--- baocaothongke.h
+++ baocaothongke.h
@@ -1,1 +1,12 @@
+#ifndef REPORT_H
+#define REPORT_H
 
+#include "../utils.h"
+#include "../invoice/invoice.h" // Cần để truy vấn hóa đơn
+
+// Khai báo hàm báo cáo
+void revenueReport(); // Thống kê doanh thu
+void productStats();  // Thống kê sản phẩm
+void customerStats(); // Thống kê khách hàng
+void reportMenu();    // Menu báo cáo
+#endif
