a344452ee0ca067efe33ebd4cd9efac51eeef0fa
hoangdinhviet <Viet.HD234047@sis.hust.edu.vn>
2025-06-05 09:57:06 +0700

    Update mainprogram.c

--- mainprogram.c
+++ mainprogram.c
@@ -2,6 +2,9 @@
 #include "manageproduct.c"
 #include "baocaothongke.h"
 #include "KHang.c"
+#include "InvoiceManagement.h"
+#include"quanlyhoadon.h"
+
 int main(){
   cout << "chào mừng đến với chương trình";
   cout << "MENU"<< \n << 
@@ -14,49 +17,8 @@ int main(){
   cin >> choice;
   switch( choice !=0){
     case 1:
-    Node* head = NULL;
-    int choice;
-
-    loadFromCSV(&head);
-
-    do {
-        printf("\n===== MENU =====\n");
-        printf("1. Thêm sản phẩm mới\n");
-        printf("2. Hiển thị danh sách sản phẩm\n");
-        printf("3. Sửa thông tin sản phẩm\n");
-        printf("4. Xóa sản phẩm\n");
-        printf("5. Lưu danh sách ra file CSV\n");
-        printf("0. Thoát\n");
-        printf("Chọn: ");
-        scanf("%d", &choice);
-        getchar();
-
-        switch (choice) {
-            case 1:
-                inputProductWithCheck(&head);
-                break;
-            case 2:
-                printProducts(head);
-                break;
-            case 3:
-                updateProduct(head);
-                break;
-            case 4:
-                deleteProduct(&head);
-                break;
-            case 5:
-                writeToCSV(head);
-                break;
-            case 0:
-                printf("Thoát chương trình.\n");
-                break;
-            default:
-                printf("Lựa chọn không hợp lệ.\n");
-        }
-    } while (choice != 0);
-
-    freeList(&head);
-    return 0;
+    productProgram();
+    break;
     case 2:
     case 3:
     case 4:
