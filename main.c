4a5d3e70af80d46be3c5106dda16f7ea8cd30a27
VIetAnh <nguyenlevietanh1003@gmail.com>
2025-06-05 07:23:28 +0700

    .

--- main.c
+++ main.c
@@ -1,35 +1,6 @@
-#include "InvoiceManagement.h"
+#include "menu.h"   // Include menu chính
 
 int main() {
-    LinkedList list;
-    initList(&list);
-    int choice;
-    
-    while (1) {
-        mainMenu();
-        scanf("%d", &choice);
-        
-        switch (choice) {
-            case 1:
-                printf("\nChuc nang quan ly khach hang chua duoc trien khai!\n");
-                break;
-            case 2:
-                printf("\nChuc nang quan ly san pham chua duoc trien khai!\n");
-                break;
-            case 3:
-                invoiceMenu(&list);
-                break;
-            case 4:
-                printf("Tam biet!\n");
-                // Giải phóng bộ nhớ trước khi thoát
-                while (!isEmpty(list)) {
-                    PNode temp = list;
-                    list = list->next;
-                    free(temp);
-                }
-                return 0;
-            default:
-                printf("Lua chon khong hop le!\n");
-        }
-    }
+    mainMenu();     // Gọi hàm menu chính
+    return 0;       // Kết thúc chương trình
 }
